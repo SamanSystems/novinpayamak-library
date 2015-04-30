3773bb9a8b90561a1181cc9ce702a9c57a716f66
arastgoo <a.rastgoo@gmail.com>
2015-04-30 22:17:03 +0430

    Fixed some PSRs and also change the methods output values in order to easily using of this class

--- src/NovinPayamak/NovinPayamak.php
+++ src/NovinPayamak/NovinPayamak.php
@@ -6,24 +6,30 @@
  */
 namespace NovinPayamak;
 
+use SoapClient;
+use SoapFault;
+
 class NovinPayamak {
     private static $webServiceUrl = 'http://novinpayamak.com/services/SMSBox/wsdl';
     private static $CISGatewebServiceUrl = 'http://novinpayamak.com/services/CISGate/wsdl';
-    private static $gatewayNumber = '';
-    private static $gatewayPassword = '';
-    private static $panelEmail = '';
-    private static $panelPassword = '';
-    public static $SMSPanelRealCredit = 0;
+    public static $gatewayNumber = '';
+    public static $gatewayPassword = '';
+    public static $panelEmail = '';
+    public static $panelPassword = '';
+    public static $messageId = 0;
+    public static $panelRealCredit = 0;
+    public static $gatewayCredit = 0;
     public static $recipients;
     public static $classifiedRecipients = [];
     public static $message;
     public static $flash = 0;
 
     // send SMS
-    public static function send() {
+    public static function sendMessage()
+    {
         try {
-            $client = new SoapClient(self::$webServiceUrl, ['encoding'=>'UTF-8', 'connection_timeout' => 5]);
-            $res = $client->Send(
+            $client = new SoapClient(self::$webServiceUrl, ['encoding' => 'UTF-8', 'connection_timeout' => 5]);
+            $response = $client->Send(
                 [
                     'Auth' => ['number' => self::$gatewayNumber, 'pass' => self::$gatewayPassword],
                     'Recipients' => self::$recipients,
@@ -32,32 +38,40 @@ class NovinPayamak {
                     'Flash' => self::$flash,
                 ]
             );
-            return $res;
-        } catch(SoapFault $e){
+            if ($response->Status == 1000) {
+                self::$messageId = $response->MessageId;
+                return true;
+            }
+        } catch(SoapFault $e) {
             // you can set log
         }
-        return [];
+        return false;
     }
 
     // check remained count of SMS on the gateway
-    public static function CheckCredit(){
+    public static function checkCredit()
+    {
         try {
-            $client = new SoapClient(self::$webServiceUrl, ['encoding'=>'UTF-8', 'connection_timeout' => 5]);
-            $credit = $client->CheckCredit(['Auth' => ['number' => self::$gatewayNumber, 'pass' => self::$gatewayPassword]]);
-            return $credit;
-        } catch(SoapFault $e){
+            $client = new SoapClient(self::$webServiceUrl, ['encoding' => 'UTF-8', 'connection_timeout' => 5]);
+            $response = $client->CheckCredit(['Auth' => ['number' => self::$gatewayNumber, 'pass' => self::$gatewayPassword]]);
+            if ($response->Status == 1000) {
+                self::$gatewayCredit = $response->Credit;
+                return true;
+            }
+        } catch(SoapFault $e) {
             // you can set log
         }
         return false;
     }
 
     // check panel credit, unit is Tooman
-    public static function CheckRealCredit(){
+    public static function checkRealCredit()
+    {
         try {
-            $client = new SoapClient(self::$CISGatewebServiceUrl, ['encoding'=>'UTF-8', 'connection_timeout' => 5]);
-            $credit = $client->CheckRealCredit(['Auth' => ['email' => self::$panelEmail, 'password' => self::$panelPassword]]);
-            if ($credit->Status == 1000) {
-                self::$SMSPanelRealCredit = $credit->Credit;
+            $client = new SoapClient(self::$CISGatewebServiceUrl, ['encoding' => 'UTF-8', 'connection_timeout' => 5]);
+            $response = $client->CheckRealCredit(['Auth' => ['email' => self::$panelEmail, 'password' => self::$panelPassword]]);
+            if ($response->Status == 1000) {
+                self::$panelRealCredit = $response->Credit;
                 return true;
             }
         } catch(SoapFault $e) {
